ef229d4ceb2404c79c73106b3b78365a66a0a684
ekgpgdi <dlekgp0423@naver.com>
2025-05-03 21:59:21 +0900

    Initial commit

---
+++ src/main/kotlin/com/dahye/usercouponservice/UserCouponServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.dahye.usercouponservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UserCouponServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<UserCouponServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/dahye/usercouponservice/UserCouponServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dahye.usercouponservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserCouponServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
